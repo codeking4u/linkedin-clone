407261a0ad246bfdd02b9f0a122dd45a8ac9476d
Amar <codeking4u@gmail.com>
2021-05-30 17:59:42 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      { /* header */}
+      <Header />
+
+      {/* app body */}
+
+      {/* sidebar */}
+
+      {/* Feed */}
+
+      {/* widgets */}
+
+    </div>
+  );
+}
+
+export default App;
